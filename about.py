cd4d31ae400d79ffe6ed92024823e7fbc0b42c09
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-06 22:54:29 +0530

    Modules

---
+++ about.py
@@ -0,0 +1,16 @@
+__help__ = """
+*Lyci - A Powerful Telegram Group Manager 🎶
+
+Powerful Abilities*
+
+• Group Voice Chat Music Play ❤
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer [𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖 •••](http://t.me/prabha_sha) 🇱🇰
+
+[GitHub](https://github.com/Prabhasha-p)"""
+
+__mod_name__ = "About 🇱🇰"
---
+++ musicplay.py
@@ -0,0 +1,14 @@
+__help__ = """
+*• Group Voice Chat Music Play 🎧 * \n
+
+In Order For Your Group Songs to Play Through Lyci Bot, You Must First Add @LyciAssistant to Your Group and be Promoted to @LyciBot Group Admin. \n \n 
+  
+/play - 🎶 Play the replied audio file or YouTube video \n
+/pause - ▶️ Pause the audio stream \n
+/resume - ⏸ Resume the audio stream \n
+/skip - ↪️ Skip the current audio stream \n
+/mute - 🔇 Mute the userbot \n
+/unmute - 🔊 Unmute the userbot \n
+/stop - 🗑🛑 Clear the queue and remove the userbot from the call"""
+
+__mod_name__ = "Music Play 🎶"
