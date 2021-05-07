fe2929d35372f6e569b98422add21b6e7b2521dc
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 19:55:16 +0530

    Update blacklistusers_sql.py

--- Sangtei/Modules/sql/blacklistusers_sql.py
+++ Sangtei/Modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from Lyci.modules.sql import BASE, SESSION
+from Sangtei.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
