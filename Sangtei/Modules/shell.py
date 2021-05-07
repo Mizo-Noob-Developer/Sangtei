6fea9633469e50bcda84a30b3f2b6cbd1d68f91f
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 19:15:22 +0530

    Update shell.py

--- Sangtei/Modules/shell.py
+++ Sangtei/Modules/shell.py
@@ -4,8 +4,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
 
-from Lyci import LOGGER, dispatcher
-from Lyci.modules.helper_funcs.chat_status import dev_plus
+from Sangtei import LOGGER, dispatcher
+from Sangtei.modules.helper_funcs.chat_status import dev_plus
 
 
 @dev_plus
