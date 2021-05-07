d14069dc52530355f2a6c28a58c1fd47d291c5db
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 20:14:34 +0530

    Update extraction.py

--- Sangtei/Modules/helper_funcs/extraction.py
+++ Sangtei/Modules/helper_funcs/extraction.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from Lyci import LOGGER
-from Lyci.modules.users import get_user_id
+from Sangtei import LOGGER
+from Sangtei.modules.users import get_user_id
 
 
 def id_from_reply(message):
