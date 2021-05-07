5ca85f328f1698a053e4c5e934889b39c1bdd715
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 20:05:49 +0530

    Update reporting_sql.py

--- Sangtei/Modules/sql/reporting_sql.py
+++ Sangtei/Modules/sql/reporting_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import Boolean, Column, Integer, String
 
-from Lyci.modules.sql import BASE, SESSION
+from Sangtei.modules.sql import BASE, SESSION
 
 
 class ReportingUserSettings(BASE):
