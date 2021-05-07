fdf6279f66652c1c5143fc7323690720a58b5a93
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 16:14:22 +0530

    Update eval.py

--- Sangtei/Modules/eval.py
+++ Sangtei/Modules/eval.py
@@ -9,8 +9,8 @@ from contextlib import redirect_stdout
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Lyci import LOGGER, dispatcher
-from Lyci.modules.helper_funcs.chat_status import dev_plus
+from Sangtei import LOGGER, dispatcher
+from Sangtei.modules.helper_funcs.chat_status import dev_plus
 
 namespaces = {}
 
