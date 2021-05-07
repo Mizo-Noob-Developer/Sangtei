f1eaf3dca388eb1e97c1a6f558e73f2a4b94262e
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 19:58:55 +0530

    Update cust_filters_sql.py

--- Sangtei/Modules/sql/cust_filters_sql.py
+++ Sangtei/Modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
-from Lyci.modules.helper_funcs.msg_types import Types
-from Lyci.modules.sql import BASE, SESSION
+from Sangtei.modules.helper_funcs.msg_types import Types
+from Sangtei.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
