2347f8345076c7b9aac0f2b9bc28246779572325
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 18:34:16 +0530

    Update math.py

--- Sangtei/Modules/math.py
+++ Sangtei/Modules/math.py
@@ -4,8 +4,8 @@ import pynewtonmath as newton
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from Lyci import dispatcher
-from Lyci.modules.disable import DisableAbleCommandHandler
+from Sangtei import dispatcher
+from Sangtei.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
