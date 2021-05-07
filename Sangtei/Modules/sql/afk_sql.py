618ac15accbd8476dd15db773cceb92364ce90c8
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 19:52:28 +0530

    Update afk_sql.py

--- Sangtei/Modules/sql/afk_sql.py
+++ Sangtei/Modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 
-from Lyci.modules.sql import BASE, SESSION
+from Sangtei.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
