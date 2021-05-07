b8f6041a01e610e581107a4586ddb3f732dbbbdc
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 18:48:34 +0530

    Update paste.py

--- Sangtei/Modules/paste.py
+++ Sangtei/Modules/paste.py
@@ -2,8 +2,8 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from Lyci import dispatcher
-from Lyci.modules.disable import DisableAbleCommandHandler
+from Sangtei import dispatcher
+from Sangtei.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
