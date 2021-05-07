a6be900115b77767004808b6d570d8be5710ecb5
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 09:35:52 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Lyci
-net start Lyci
+net stop Sangtei
+net start Sangtei
