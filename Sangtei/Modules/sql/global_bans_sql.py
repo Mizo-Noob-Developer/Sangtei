2368227c27d8845d0c7b254d79eb1fdeb2f6d801
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-07 20:02:38 +0530

    Update global_bans_sql.py

--- Sangtei/Modules/sql/global_bans_sql.py
+++ Sangtei/Modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
-from Lyci.modules.sql import BASE, SESSION
+from Sangtei.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
