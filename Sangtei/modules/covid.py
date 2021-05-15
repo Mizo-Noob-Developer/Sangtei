bb871adcf395d3c0e1f5e44e87059422e645c250
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-05-15 10:55:55 +0530

    Update covid.py

--- Sangtei/modules/covid.py
+++ Sangtei/modules/covid.py
@@ -11,20 +11,13 @@ def covid(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)
     if len(text) == 1:
-        r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        r = requests.get("https://api.covid19india.org/state_district_wise.json").json()
+        reply_text = f"**State Cases** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
         reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
-   else:
-      import requests
-url = "https://api.covid19india.org/state_district_wise.json"
-payload={}
-headers = {}
-response = requests.request("GET", url, headers=headers, data=payload)
-print(response.text)
 
 COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
 dispatcher.add_handler(COVID_HANDLER)
