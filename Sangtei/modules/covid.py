ad6773a7028344f253f0a63d3543855e69533ee8
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-06-18 19:01:07 +0530

    Update covid.py

--- Sangtei/modules/covid.py
+++ Sangtei/modules/covid.py
@@ -1,13 +1,12 @@
+import json
+import requests
+from urllib.request import urlopen
 from telegram import ParseMode, Update, Bot, Chat
 from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
 from Sangtei import dispatcher
-
-import requests
-
-import json
-from urllib.request import urlopen
-
+from Sangtei.modules.disable import DisableAbleCommandHandler
+from requests import get
 
 def covindia(bot: Bot, update: Update):
     message = update.effective_message
@@ -31,25 +30,52 @@ def covindia(bot: Bot, update: Update):
     if state:
         bot.send_message(
             message.chat.id,
-            '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
+            '`COVID-19`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
     else:
         bot.send_message(
             message.chat.id,
-            'You need to specify a valid Indian state!!',
+            'You need to specify a valid Indian state!',
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
+@run_async
+def corona(bot: Bot, update: Update):
+    message = update.effective_message
+    device = message.text[len('/corona '):]
+    fetch = get(f'https://coronavirus-tracker-api.herokuapp.com/all')
+
+    if fetch.status_code == 200:
+        usr = fetch.json()
+        data = fetch.text
+        parsed = json.loads(data)
+        total_confirmed_global = parsed["latest"]["confirmed"]
+        total_deaths_global = parsed["latest"]["deaths"]
+        total_recovered_global = parsed["latest"]["recovered"]
+        active_cases_covid19 = total_confirmed_global - total_deaths_global - total_recovered_global
+        reply_text = ("*Corona Stats🦠:*\n"
+        "Total Confirmed: `" + str(total_confirmed_global) + "`\n"
+        "Total Deaths: `" + str(total_deaths_global) + "`\n"
+        "Total Recovered: `" + str(total_recovered_global) +"`\n"
+        "Active Cases: `"+ str(active_cases_covid19) + "`")
+        message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+        return
+
+    elif fetch.status_code == 404:
+        reply_text = "The API is currently down."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 __help__ = """
- 
+ - /covid: get worldwide corona status
  - /covindia <state>: Get real time COVID-19 stats for the input Indian state
 """
 
-__mod_name__ = 'COVID-19 VIRUS'
+__mod_name__ = 'COVID-19'
 
 COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
-
+CORONA_HANDLER = DisableAbleCommandHandler("covid", corona, admin_ok=True)
+dispatcher.add_handler(CORONA_HANDLER)
 dispatcher.add_handler(COV_INDIA_HANDLER)
