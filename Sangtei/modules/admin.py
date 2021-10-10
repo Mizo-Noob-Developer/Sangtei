5320b86be10caa06d479178097ef387b03bdbd4b
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-10 11:50:40 +0530

    Update admin.py

--- Sangtei/modules/admin.py
+++ Sangtei/modules/admin.py
@@ -222,8 +222,7 @@ def set_title(update: Update, context: CallbackContext):
 
     if user_id == bot.id:
         message.reply_text(
-            "Keimah leh keimah hming lem ka in pe thei lo! Admin a min siamtu admin hnen ah khan va sawi rawh."
-       )
+            "Keimah leh keimah hming lem ka in pe thei lo! Admin a min siamtu admin hnen ah khan va sawi rawh.")
        return
 
     if not title:
