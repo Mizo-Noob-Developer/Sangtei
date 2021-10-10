d440a0fb1eacd68c0d90ddfd9196d512873a0d62
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-11 00:00:33 +0530

    Update nightmode.py

--- Sangtei/modules/nightmode.py
+++ Sangtei/modules/nightmode.py
@@ -310,7 +310,7 @@ async def _(event):
         print(e)
 
 __help__ = ""
-**Nightmode**
+*• Nightmode*
 • /setnightmode <time zone> i awmna bial hun bithliah siamna.
 • /Nightmode <on/off/enable> nighmode on leh off na tur
 __mode_name__ = "Nighmode"
