813a7f2babba3652a67f5a03e468894ffd8d63c6
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-12 08:50:20 +0530

    Update night_mode_sql.py

--- Sangtei/modules/sql/night_mode_sql.py
+++ Sangtei/modules/sql/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from Sangtei.modules.sql import BASE, SESSION
+from Sangtei.modules.sql.nightmode_sql import BASE, SESSION
 
 
 class Nightmode(BASE):
