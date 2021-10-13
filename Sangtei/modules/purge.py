62e78e9dd8a7e6ab6a4db4dc03f98e6db2177b89
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-13 16:04:02 +0530

    Update purge.py

--- Sangtei/modules/purge.py
+++ Sangtei/modules/purge.py
@@ -18,16 +18,16 @@ async def purge_messages(event):
     if not await user_is_admin(
         user_id=event.sender_id, message=event
     ) and event.from_id not in [1087968824]:
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("He command hi Admin te tan chiah hman phal ani")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("He message hi purge theih ani tlat lo")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply("Khawi atanga purge tur nge hre tur in message reply rawh.")
         return
     messages = []
     message_id = reply_msg.id
@@ -42,7 +42,7 @@ async def purge_messages(event):
 
     await event.client.delete_messages(event.chat_id, messages)
     time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    text = f"Purge zawh fel ani e, hun hman rei zawng {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode="markdown")
 
 
@@ -54,16 +54,16 @@ async def delete_messages(event):
     if not await user_is_admin(
         user_id=event.sender_id, message=event
     ) and event.from_id not in [1087968824]:
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("He command hi Admin te tan chiah hman phal ani")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("He message hi paih theih ni in a lang lo?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("What You want to delete?")
+        await event.reply("Eng nge paih i duh chu?")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
@@ -71,10 +71,10 @@ async def delete_messages(event):
 
 
 __help__ = """
-*Admin only:*
- •  /del : deletes the message you replied to
- •  /purge : deletes all messages between this and the replied to message.
- •  /purge `<integer X>`: deletes the replied message, and X messages following it if replied to a message.
+*Admin tan chauh:*
+ •  /del : i message reply te paih na
+ •  /purge : message zawng zawng paihna leh a inkar ami zawng then fai na.
+ •  /purge `<integer X>`: message paih ani e, chuan i message thlan chhuah a reply na awm zawng zawng pawh paih ani.
 """
 
 __mod_name__ = "Purges"
