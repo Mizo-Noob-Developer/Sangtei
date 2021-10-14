f52c1910ee9b7f6d20ab14a32f6ad0bf372df0ec
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-14 21:49:04 +0530

    Update dev.py

--- Sangtei/modules/dev.py
+++ Sangtei/modules/dev.py
@@ -19,10 +19,10 @@ def leave(update: Update, context: CallbackContext):
         chat_id = str(args[0])
         try:
             bot.leave_chat(int(chat_id))
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("Beep boop, He group hi ka chhuahsan e!.")
         except TelegramError:
             update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho)."
+                "Beep boop, he group hi ka chhuahsan thei lo(a chhan pawh ka hre lo)."
             )
     else:
         update.effective_message.reply_text("Send a valid chat ID")
@@ -32,11 +32,11 @@ def leave(update: Update, context: CallbackContext):
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
     sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart."
+        "Thlak danglam na chu a bul thut atangin neih ani, chuan tih nun leh tuma beih mek ani."
     )
     subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
 
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+    sent_msg_text = sent_msg.text + "\n\nThlak danglam mek ani...Ka ngaihdan in.. Tih nun leh mek ani ang "
 
     for i in reversed(range(5)):
         sent_msg.edit_text(sent_msg_text + str(i + 1))
@@ -52,7 +52,7 @@ def gitpull(update: Update, context: CallbackContext):
 @dev_plus
 def restart(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one"
+        "Hmanhmawh tak in thil thar chu dah ani a, chuan a hlui zawk hi tih thih ani ang"
     )
 
     os.system("restart.bat")
