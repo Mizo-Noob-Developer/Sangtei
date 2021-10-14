440cfab752ff8857223382f5602967511e7b0c56
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-14 14:49:34 +0530

    Create feedback.py

---
+++ Sangtei/modules/feedback.py
@@ -0,0 +1,44 @@
+import html
+from telegram import Update, Bot, ParseMode
+from telegram.ext import run_async
+from Sangtei.modules.disable import DisableAbleCommandHandler
+from Sangtei import dispatcher
+from requests import get
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@run_async
+def feedback(bot: Bot, update: Update):
+  name = update.effective_message.from_user.first_name
+  message = update.effective_message
+  userid=message.from_user.id
+  text = message.text[len('/feedback '):]
+   
+
+  feed_text = f"Sangtei feedback *Thar* a dawng e, he user [{name}](tg://user?id={userid}) hen atang hian.\n\nfeed: {text}"
+  
+
+  bot.send_message(-1001199155295, feed_text, parse_mode=ParseMode.MARKDOWN)
+ 
+  text = html.escape(text)
+  reply_text=f"Feedback min pek avang in kan lawm e."
+  message.reply_text(reply_text, reply_markup=InlineKeyboardMarkup(
+                                                [[InlineKeyboardButton(text="I feedback chu hetah hian i hmu thei ang",url="https://t.me/Sangtei_Update")]]))
+                                               
+  
+
+  
+
+
+
+help = """
+ - /feedback : You can give us your feedbacks 
+               can can see your feeds here.
+"""
+
+mod_name = "FEEDBACK"
+
+feed_handle = DisableAbleCommandHandler("feedback", feedback)
+
+dispatcher.add_handler(feed_handle)
