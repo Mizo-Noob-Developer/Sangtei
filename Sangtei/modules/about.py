04e423510f2de3a377de11389c0a722795810e95
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-10-17 14:23:39 +0530

    Update about.py

--- Sangtei/modules/about.py
+++ Sangtei/modules/about.py
@@ -1,16 +1,23 @@
 __help__ = """
-*Sangtei - A Powerful Telegram Group Manager 🎶
+*Sangtei - A Powerful Telegram Group Manager.
 
 Powerful Abilities*
 
-• Group Voice Chat Music Play ❤
+• Sangtei hi Group enkawl tu tur atan a tha leh tangkai tak ani a
+  Thil tam tak tih theih a nei bawk ani.
 
-• File To Link and URL Upload 📂
+• Sangtei hian I group tihbuai tum tu te lak ah a venghim thei bawk che.
+  Tha i ti a, i lo hmang tangkai anih chuan, min lo theh darh sak ve dawn nia.
 
-• Youtube Downloader 🎵
+• Tin, ka  bot hi tun dinhmun ah, free host atanga run ani a.
+  Tlem in a muan mai bak ah, Bot ah hian modules tangkai zual tur te dah a rem thin lo a,
+  Hma kan sawn theih zel i duh a, min tanpui ve i duh chuan
+ A hnuai a, Developer hnen ah khuan message i thawn dawn nia.
 
-Developer [Nicky Lalrochhara](http://t.me/Nickylrca) 🇮🇳
+Developer [Nicky Lalrochhara](http://t.me/Nickylrca)
 
-[Sangtei Music Player🎵](https://t.me/sangteibot)"""
+Chuan, group voicechat lam ah hla play na bot kan siam tel bawk a.
+A hnuai ami hi i group lam ah i lo add ve dawn nia.
+[Sangtei Music Player](https://t.me/sangteibot)"""
 
 __mod_name__ = "About"
